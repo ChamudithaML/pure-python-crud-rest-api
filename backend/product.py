41e9ea0dbb781287f5850870aae4ff15eb8cfa27
ChamudithaML <lilan.2019660@iit.ac.lk>
2024-10-08 00:40:57 +0530

    add product handling

---
+++ backend/product.py
@@ -0,0 +1,34 @@
+products = {}
+
+def create_product(name, description, stock_count):
+    if name in products:
+        return "Product already exists."
+    
+    products[name] = {
+        "description": description,
+        "stock_count": stock_count
+    }
+    
+    return "Product created successfully."
+
+def update_product_stock(name, amount):
+    if name not in products:
+        return "Product does not exist."
+    
+    try:
+        amount = int(amount)
+    except ValueError:
+        return "Invalid stock count value. It must be an integer."
+
+    products[name]['stock_count'] += amount
+    return "Product stock updated successfully."
+
+def view_products():
+    return products
+
+def delete_product(name):
+    if name not in products:
+        return "Product does not exist."
+    
+    del products[name]
+    return "Product deleted successfully."
